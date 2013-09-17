9459481279660651e2fde32d8d30de80c1f66199
sinholic <mahar@gaze.asia>
2013-09-17 12:31:19 +0700

    Initial Commit

---
+++ dixonsgame/DGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DGAppDelegate.h
+//  dixonsgame
+//
+//  Created by Mahar Prasetio on 9/17/13.
+//  Copyright (c) 2013 gaze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dixonsgame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dixonsgame
+//
+//  Created by Mahar Prasetio on 9/17/13.
+//  Copyright (c) 2013 gaze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DGAppDelegate class]));
+    }
+}
